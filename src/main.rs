e2eb187acacb2632a1da2aa270bb0c11ced0d5cf
Olivier Despesse <olived19@gmail.com>
2020-01-26 11:07:06 +0100

    Initialize the empty bitfield

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,19 @@ struct BloomFilter {
 
 impl BloomFilter {
     fn new(capacity: u32, err_rate: f32) -> Self {
+        if capacity == 0 {
+            panic!("Invalid Bloom filter capacity: cannot be 0");
+        }
         let factor = (1.0/2.0_f32.powf(2.0_f32.ln())).ln();
         let bitfield_size = ((capacity as f32 * err_rate.ln()) / factor).ceil();
         let key_size = ((bitfield_size / capacity as f32) * 2.0_f32.ln()).ceil();
+        let mut bitfield = Vec::with_capacity(bitfield_size as usize);
+        for _ in 0..(bitfield_size as usize) {
+            bitfield.push(false);
+        }
         BloomFilter {
             key_size: key_size as u32,
-            bitfield: Vec::with_capacity(bitfield_size as usize)
+            bitfield
         }
     }
 
@@ -35,5 +42,35 @@ impl BloomFilter {
             result.push(position);
         }
         result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_bloom_filter() {
+        let filter = BloomFilter::new(1000, 0.1);
+        for bit in filter.bitfield {
+            assert!(!bit);
+        }
+    }
+
+    #[test]
+    fn check_bloom_filter_configuration() {
+        let filter = BloomFilter::new(5, 0.1);
+        assert_eq!(4, filter.key_size);
+        assert_eq!(24, filter.bitfield.len());
+
+        let filter = BloomFilter::new(100, 0.5);
+        assert_eq!(2, filter.key_size);
+        assert_eq!(145, filter.bitfield.len());
+    }
+
+    #[test]
+    #[should_panic]
+    fn check_invalid_bloom_filter_capacity() {
+        BloomFilter::new(0, 1.0);
     }
 }
